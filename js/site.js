33dcabf4959b7c9eaa5d91033d19041857b8a32c
Chris <rikoukuroi@yahoo.com>
2021-08-17 12:27:37 -0400

    v1

--- js/site.js
+++ js/site.js
@@ -1,4 +1,33 @@
-//Call hello world
-function helloWorld() {
-    alert("hellow world");
+//Get the values from the page.
+//control
+function getString() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+    //Reverse the string
+//logic
+function reverseString(userString) {
+    let revString = [];
+
+
+
+    //reverse it using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+    return revString
+}
+//display the string in the p tag we made
+//view result
+function displayString() {
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is:  ${revString}`;
+
+
+    //show the box
+    document.getElementById("alert").classList.remove("invisible");
+
+}
 }
